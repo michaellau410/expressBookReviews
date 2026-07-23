9fbc72bd962adf682a20ff5134b7ba2ca24eea2c
Michael Lau <michael.lau.410@gmail.com>
2026-07-23 08:17:47 -0400

    update

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,26 +5,62 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-
+const isValid = (username) => {
     let matched_username = users.find( (user) =>  user.username === username);
-
     if(matched_username)
         return false;
     else
         return true;
-
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password) => {
+
+    let matched = users.find( (user) =>  user.username === username && user.password === password);
+    if(matched)
+        return true;
+    else
+        return false;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    console.log("just call login");
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if(! (username && password))
+    {
+        console.log("Missing login info");
+        return res.status(300).json({message: "Missing login info"});
+    }
+
+    
+    if( authenticatedUser(username, password))
+    {
+        console.log("correct login info");
+
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+
+        return res.status(200).send("User successfully logged in");
+    }
+    else
+    {
+        console.log("incorrect login info");
+        return res.status(300).json({message: "Incorrect login info"});
+    }
+    
+
+    //return res.status(300).json({message: "login ... ???"});
+
 });
 
 // Add a book review
