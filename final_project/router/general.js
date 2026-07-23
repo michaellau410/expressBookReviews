dcc05526f3423f4be1be2bda4148aa13b164de8b
Michael Lau <michael.lau.410@gmail.com>
2026-07-23 00:15:51 -0400

    update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,48 +5,42 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.post("/register", (req, res) => {
+    //Write your code here
+    return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  //return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
     let book_list = JSON.stringify(books, null, 4);
-
-  return res.send(book_list);
-
+    return res.send(book_list);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', function (req, res) {
     let isbn = req.params.isbn;
-    let selected_books = Object.entries(books).filter( ([id, detail]) => id === isbn );
-    return res.send(JSON.stringify(selected_books, null, 4) );
- });
-  
+    let selected_books = Object.entries(books).filter(([id, detail]) => id === isbn);
+    return res.send(JSON.stringify(selected_books, null, 4));
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-
+public_users.get('/author/:author', function (req, res) {
     let author = req.params.author.toLowerCase();
-  let selected_books = Object.entries(books).filter( ([id, detail]) => detail.author.toLowerCase() === author);
-  return res.send(JSON.stringify(selected_books, null, 4) );
-
-
+    let selected_books = Object.entries(books).filter(([id, detail]) => detail.author.toLowerCase() === author);
+    return res.send(JSON.stringify(selected_books, null, 4));
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+    let title = req.params.title.toLowerCase();
+    let selected_books = Object.entries(books).filter(([id, detail]) => detail.title.toLowerCase() === title);
+    return res.send(JSON.stringify(selected_books, null, 4));
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+    //Write your code here
+    return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.general = public_users;
